406aed7b6003adf26298e2d4b9f9813209abc40a
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2016-10-07 11:37:18 +0200

    Template based subscription

--- client/recipes/recipes.js
+++ client/recipes/recipes.js
@@ -1,4 +1,8 @@
-Meteor.subscribe('recipes');
+Template.Recipes.onCreated(function () {
+  this.autorun(() => {
+    this.subscribe('recipes');
+  })
+});
 
 Template.Recipes.helpers({
   recipes: () => {
